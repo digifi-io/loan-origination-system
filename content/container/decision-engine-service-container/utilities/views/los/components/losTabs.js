bdf6ca499b7deb7c68250b4d03e636b6f87b820a
Cody Hahn <cody@digifi.io>
2019-10-14 17:24:47 -0400

    Remove other communications and documents. Update losTabs

--- content/container/decision-engine-service-container/utilities/views/los/components/losTabs.js
+++ content/container/decision-engine-service-container/utilities/views/los/components/losTabs.js
@@ -16,10 +16,6 @@ module.exports = function (tabname) {
       location: 'applicationsdashboard',
       icon: 'file alternate',
     }, {
-      label: 'Tasks',
-      location: 'tasks',
-      icon: 'check square',
-    }, {
       label: 'Customers',
       icon: 'user',
       dropdown: [{
@@ -37,38 +33,35 @@ module.exports = function (tabname) {
       location: 'intermediaries',
       icon: 'fas fa-seedling',
     }, {
+      label: 'Tasks',
+      location: 'tasks',
+      icon: 'check square',
+    }, {
       label: 'Reporting',
       location: 'reporting',
       icon: 'fas fa-robot',
     }, {
-      label: 'Task Bots',
-      location: 'taskbots',
-      icon: 'fas fa-robot',
-    }, {
-      label: 'Other',
+      label: 'Settings',
       icon: 'ellipsis horizontal',
       dropdown: [{
-        name: 'Communications',
-        location: '/los/others/communications',
+        name: 'Workflow Configuration',
+        location: '/los/settings/applicationstatuses',
       }, {
-        name: 'Documents',
-        location: '/los/others/docs',
-      }, {
-        name: 'Loan Product Types',
-        location: '/los/others/products',
-      }, {
-        name: 'Customer & Intermediary Templates',
-        location: '/los/others/customer_templates',
-      }, {
-        name: 'Document Creation Templates',
-        location: '/los/others/templates',
+        name: 'Task Automation Bots',
+        location: '/los/settings/taskbots',
       }, {
         name: 'Application Labels',
-        location: '/los/others/applicationlabels',
+        location: '/los/settings/applicationlabels',
       }, {
-        name: 'Application Statuses & Rejection Types',
-        location: '/los/others/applicationstatuses',
-      },
+        name: 'Document Creation Templates',
+        location: '/los/settings/templates',
+      }, {
+        name: 'Loan Product Templates',
+        location: '/los/settings/products',
+      }, {
+        name: 'Customer & Intermediary Templates',
+        location: '/los/settings/customer_templates',
+      }, 
       ],
     },
   ];
--- content/container/decision-engine-service-container/views/los/others/applicationlabels.manifest.js
+++ content/container/decision-engine-service-container/views/los/others/applicationlabels.manifest.js
@@ -12,7 +12,7 @@ let randomKey = Math.random;
 
 module.exports = {
   containers: {
-    '/los/others/applicationlabels': {
+    '/los/settings/applicationlabels': {
       layout: {
         component: 'div',
         privileges: [ 101, 102, 103 ],
@@ -20,7 +20,7 @@ module.exports = {
           style: styles.pageContainer,
         },
         children: [
-          losTabs('Other'),
+          losTabs('Settings'),
           plainHeaderTitle({
             title: 'Application Labels',
           }),
