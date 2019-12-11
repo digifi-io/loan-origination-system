c324156ef4dc0cf18479509d5e3ed3195c060fce
Dmitry Khakhomov <dmitry.khakhomov@satelliteinnovations.io>
2019-12-11 16:47:26 +0300

    [DIG-64] Added organization data save permission check for file creation

--- content/container/decision-engine-service-container/controllers/file.js
+++ content/container/decision-engine-service-container/controllers/file.js
@@ -20,24 +20,26 @@ function createFiles(req, res, next) {
   let user = req.user;
   let organization = (user && user.association && user.association.organization && user.association.organization._id) ? user.association.organization._id : 'organization';
   const File = periodic.datas.get('standard_file');
-  req.controllerData.creditEngineResponse.forEach(async result => {
-    if (result.data_sources && result.data_sources.length) {
-      result.data_sources.forEach(async (data_source) => {
-        let { name, data, } = data_source;
-        let Key, filetype;
-        try {
-          JSON.parse(data);
-          Key = `dataintegrations/api_response_${new Date()}_${uniqid()}/${name.replace(/\//g, '_')}.json`;
-          filetype = 'json';
-        } catch (e) {
-          Key = `dataintegrations/api_response_${new Date()}_${uniqid()}/${name.replace(/\//g, '_')}.xml`;
-          filetype = 'xml';
-        }
-        await helpers.uploadAWS({ Key, Body: data, });
-        await File.create({ newdoc: Object.assign({}, { name, fileurl: Key, filetype, organization, user, }), });
-      });
-    }
-  });
+  if (req.controllerData.org && req.controllerData.org.save_data) {
+    req.controllerData.creditEngineResponse.forEach(async result => {
+      if (result.data_sources && result.data_sources.length) {
+        result.data_sources.forEach(async (data_source) => {
+          let { name, data, } = data_source;
+          let Key, filetype;
+          try {
+            JSON.parse(data);
+            Key = `dataintegrations/api_response_${new Date()}_${uniqid()}/${name.replace(/\//g, '_')}.json`;
+            filetype = 'json';
+          } catch (e) {
+            Key = `dataintegrations/api_response_${new Date()}_${uniqid()}/${name.replace(/\//g, '_')}.xml`;
+            filetype = 'xml';
+          }
+          await helpers.uploadAWS({ Key, Body: data, });
+          await File.create({ newdoc: Object.assign({}, { name, fileurl: Key, filetype, organization, user, }), });
+        });
+      }
+    });
+  }
   return next();
 }
 
