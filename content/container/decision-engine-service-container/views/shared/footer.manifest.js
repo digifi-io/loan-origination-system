ddbce95d774ae74b3cf44fb2a88922c3259c3c95
Cody Hahn <cody@digifi.io>
2019-12-11 16:56:23 +0300

    Bump LOS version to 1.4.1

--- content/container/decision-engine-service-container/views/shared/footer.manifest.js
+++ content/container/decision-engine-service-container/views/shared/footer.manifest.js
@@ -32,7 +32,7 @@ module.exports = {
               margin: '0 auto',
             },
           },
-          children: `© ${currentYear} ${THEMESETTINGS.footer.company_name || 'DigiFi, Inc.'} Version 1.4.0`,
+          children: `© ${currentYear} ${THEMESETTINGS.footer.company_name || 'DigiFi, Inc.'} Version 1.4.1`,
         },
         ],
       },
