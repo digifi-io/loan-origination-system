a408fe4df85444af8d0611634c7563d0a2c5fa7f
alanuraz <alanuraz@gmail.com>
2020-01-30 11:17:31 -0500

    Move AWS initialization to AmazonCloud class

---
+++ content/container/decision-engine-service-container/cloud/index.js
@@ -0,0 +1,106 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+const periodic = require('periodicjs');
+const THEMESETTINGS = periodic.settings.container['decision-engine-service-container'];
+const machineLearningSettings = periodic.settings.container['decision-engine-service-container'].machinelearning;
+const utilities = require('../utilities');
+const mlcrons = utilities.mlcrons;
+const Promisie = require('promisie');
+
+class AmazonCloud {
+  constructor() {
+    this.AWS = AWS;
+    this.updateConfigs();
+    this.useMLCrons();
+  }
+  
+  updateConfigs() {
+    const aws_configs = periodic.settings.extensions['periodicjs.ext.packagecloud'].client;
+    AWS.config.update({
+      region: aws_configs.region,
+    });
+    AWS.config.setPromisesDependency(require('promisie'));
+    periodic.aws = {};
+    AWS.config.credentials = new AWS.Credentials(THEMESETTINGS.sagemaker.key, THEMESETTINGS.sagemaker.secret, null);
+    periodic.aws.sagemaker = new AWS.SageMaker();
+    periodic.aws.sagemakerruntime = new AWS.SageMakerRuntime();
+    periodic.aws.sagemaker_bucket = THEMESETTINGS.sagemaker.bucket;
+    AWS.config.credentials = new AWS.Credentials(aws_configs.accessKeyId, aws_configs.accessKey, null);
+    periodic.aws.s3 = new AWS.S3();
+    periodic.aws.machinelearning = new AWS.MachineLearning();
+  }
+
+  useMLCrons() {
+    if (!machineLearningSettings.use_mlcrons) return;
+    const machinelearning = periodic.aws.machinelearning;
+    const s3 = periodic.aws.s3;
+    mlcrons.sageMaker();
+    mlcrons.digifi();
+    mlcrons.modelSelectionUpdater();
+
+    setInterval(() => {
+      mlcrons.overallModelUpdater();
+    }, 5000);
+
+    setInterval(async () => {
+      const redisClient = periodic.app.locals.redisClient;
+      const getAllKeys = Promisie.promisify(redisClient.keys).bind(redisClient);
+      const getValues = Promisie.promisify(redisClient.hgetall).bind(redisClient);
+      const allKeys = await getAllKeys(`${periodic.environment}_ml_preprocessing:*`);
+      const allValues = await Promise.all(allKeys.map(key => getValues(key)));
+      allValues.forEach(mlmodel => {
+        mlcrons.preTrainingProcess({ mlmodel, });
+        redisClient.del(`${periodic.environment}_ml_preprocessing:${mlmodel._id.toString()}`);
+      });
+    }, 15000);
+    
+    setInterval(() => {
+      periodic.app.locals.redisClient.keys(`${periodic.environment}_ml_aws:*`, function (err, model_keys) {
+        if (model_keys.length) {
+          let data_sources = {};
+          let ml_models = {};
+          let upload_datasources = {};
+          let getValueAsync = (model_key) => new Promise((resolve, reject) => {
+            try {
+              periodic.app.locals.redisClient.hgetall(model_key, function (err, results) {
+                return resolve(results);
+              });
+            } catch (err) {
+              return reject(err);
+            }
+          });
+          let getValues = model_keys.map(model_key => getValueAsync(model_key));
+          Promise.all(getValues)
+            .then(values => {
+              model_keys.forEach((model_key, idx) => {
+                let current_model = values[idx];
+                if (current_model && current_model.status === 'datasource_cleanup' && !current_model.data_source_training_status) {
+                  upload_datasources[current_model._id] = current_model;
+                }
+                if (current_model && !current_model.model_id) return;
+                if (current_model && current_model.data_source_training_status === 'true' && current_model.data_source_testing_status === 'true' && current_model.ml_model_status === 'false') {
+                  ml_models[current_model.model_id] = current_model;
+                } else if (current_model && (current_model.data_source_training_status === 'false' || current_model.data_source_testing_status === 'false')) {
+                  data_sources[current_model.model_id] = current_model;
+                } else if (current_model && current_model.data_source_training_status === 'true' && current_model.data_source_testing_status === 'true' && current_model.ml_model_status === 'true') {
+                  return;
+                }
+                mlcrons.checkDataSources(machinelearning, data_sources);
+                mlcrons.checkMLModels(machinelearning, ml_models);
+                mlcrons.batchUpdater({ s3, machinelearning, });
+                mlcrons.modelUpdater();
+              });
+            });
+        } else {
+          mlcrons.batchUpdater({ s3, machinelearning, });
+          mlcrons.modelUpdater();
+        }
+      });
+    }, machineLearningSettings.cron_interval || 60000);
+  }  
+}
+
+module.exports = {
+  AmazonCloud,
+};
--- content/container/decision-engine-service-container/index.js
+++ content/container/decision-engine-service-container/index.js
@@ -14,8 +14,8 @@ const exec = require('child_process').exec;
 const THEMESETTINGS = periodic.settings.container[ 'decision-engine-service-container' ];
 const redis = require('redis');
 const CREDIT_PIPELINE = require('@digifi-los/credit-process');
-const AWS = require('aws-sdk');
 const util = require('util');
+const AmazonCloud = require('./cloud').AmazonCloud;
 let os = require('os');
 const mongodb = require('mongodb');
 const streamBuffers = require('stream-buffers');
@@ -78,22 +78,12 @@ module.exports = () => {
     httpsServer.keepAliveTimeout = 0;
     periodic.servers.set('http', httpServer);
     periodic.servers.set('https', httpsServer);
+    const configs = JSON.parse(process.env.DB_CONFIG);
+    if (configs.cloud === 'AWS') {
+      periodic.cloud = new AmazonCloud();
+    }
 
-    let machineLearningSettings = periodic.settings.container[ 'decision-engine-service-container' ].machinelearning;
     let googleVisionSettings = periodic.settings.container[ 'decision-engine-service-container' ].googlevision;
-    let aws_configs = periodic.settings.extensions[ 'periodicjs.ext.packagecloud' ].client;
-    AWS.config.update({
-      region: aws_configs.region,
-    });
-    AWS.config.setPromisesDependency(require('promisie'));
-    periodic.aws = {};
-    AWS.config.credentials = new AWS.Credentials(THEMESETTINGS.sagemaker.key, THEMESETTINGS.sagemaker.secret, null);
-    periodic.aws.sagemaker = new AWS.SageMaker();
-    periodic.aws.sagemakerruntime = new AWS.SageMakerRuntime();
-    periodic.aws.sagemaker_bucket = THEMESETTINGS.sagemaker.bucket;
-    AWS.config.credentials = new AWS.Credentials(aws_configs.accessKeyId, aws_configs.accessKey, null);
-    let s3 = new AWS.S3();
-    periodic.aws.s3 = s3;
     window.process = global.process;
     vision = require('@google-cloud/vision').v1p3beta1;
     periodic.googlevision = new vision.ImageAnnotatorClient({
@@ -103,72 +93,6 @@ module.exports = () => {
       },
       projectId: googleVisionSettings.project_id,
     });
-
-    let machinelearning = new AWS.MachineLearning();
-    periodic.aws.machinelearning = machinelearning;
-
-    if (machineLearningSettings.use_mlcrons) {
-      mlcrons.sageMaker();
-      mlcrons.digifi();
-      mlcrons.modelSelectionUpdater();
-      setInterval(() => {
-        mlcrons.overallModelUpdater();
-      }, 5000);
-      setInterval(async () => {
-        const redisClient = periodic.app.locals.redisClient;
-        const getAllKeys = Promisie.promisify(redisClient.keys).bind(redisClient);
-        const getValues = Promisie.promisify(redisClient.hgetall).bind(redisClient);
-        const allKeys = await getAllKeys(`${periodic.environment}_ml_preprocessing:*`);
-        const allValues = await Promise.all(allKeys.map(key => getValues(key)));
-        allValues.forEach(mlmodel => {
-          mlcrons.preTrainingProcess({ mlmodel, });
-          redisClient.del(`${periodic.environment}_ml_preprocessing:${mlmodel._id.toString()}`);
-        });
-      }, 15000);
-      setInterval(() => {
-        periodic.app.locals.redisClient.keys(`${periodic.environment}_ml_aws:*`, function (err, model_keys) {
-          if (model_keys.length) {
-            let data_sources = {};
-            let ml_models = {};
-            let upload_datasources = {};
-            let getValueAsync = (model_key) => new Promise((resolve, reject) => {
-              try {
-                periodic.app.locals.redisClient.hgetall(model_key, function (err, results) {
-                  return resolve(results);
-                });
-              } catch (err) {
-                return reject(err);
-              }
-            });
-            let getValues = model_keys.map(model_key => getValueAsync(model_key));
-            Promise.all(getValues)
-              .then(values => {
-                model_keys.forEach((model_key, idx) => {
-                  let current_model = values[ idx ];
-                  if (current_model && current_model.status === 'datasource_cleanup' && !current_model.data_source_training_status) {
-                    upload_datasources[ current_model._id ] = current_model;
-                  }
-                  if (current_model && !current_model.model_id) return;
-                  if (current_model && current_model.data_source_training_status === 'true' && current_model.data_source_testing_status === 'true' && current_model.ml_model_status === 'false') {
-                    ml_models[ current_model.model_id ] = current_model;
-                  } else if (current_model && (current_model.data_source_training_status === 'false' || current_model.data_source_testing_status === 'false')) {
-                    data_sources[ current_model.model_id ] = current_model;
-                  } else if (current_model && current_model.data_source_training_status === 'true' && current_model.data_source_testing_status === 'true' && current_model.ml_model_status === 'true') {
-                    return;
-                  }
-                  mlcrons.checkDataSources(machinelearning, data_sources);
-                  mlcrons.checkMLModels(machinelearning, ml_models);
-                  mlcrons.batchUpdater({ s3, machinelearning, });
-                  mlcrons.modelUpdater();
-                });
-              });
-          } else {
-            mlcrons.batchUpdater({ s3, machinelearning, });
-            mlcrons.modelUpdater();
-          }
-        });
-      }, machineLearningSettings.cron_interval || 60000);
-    }
 
     exec('npm run sass', (err, stdout, stderr) => {
       if (err) logger.error('Unable to npm run sass', err);
